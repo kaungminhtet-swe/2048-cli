0fdba3af7e34ab5f7bca4ad35c79f6631c8661eb
Kaung Min Htet <kgminhtet.dev@gmail.com>
2024-03-05 07:25:20 +0630

    Added Action type in action package

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -4,3 +4,25 @@ const (
 	EMPTYCELL = iota
 	CELL
 )
+
+type Action struct{}
+
+func (a Action) Top(input [4][4]int) [4][4]int {
+	return topAction(input)
+}
+
+func (a Action) Right(input [4][4]int) [4][4]int {
+	return rightAction(input)
+}
+
+func (a Action) Bottom(input [4][4]int) [4][4]int {
+	return bottomAction(input)
+}
+
+func (a Action) Left(input [4][4]int) [4][4]int {
+	return leftAction(input)
+}
+
+func New() *Action {
+	return &Action{}
+}
